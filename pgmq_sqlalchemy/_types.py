f67ff9fe9484344f92d94ff4c9996e1b98a374bd
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-09-11 10:27:45 +0800

    feat: add db api interface

--- pgmq_sqlalchemy/_types.py
+++ pgmq_sqlalchemy/_types.py
@@ -1,4 +1,6 @@
-from typing import Union
+from typing import Union, Any
+from collections.abc import Mapping, Sequence
+from ._db_api_interface import DBAPICursor
 
 from sqlalchemy.orm import Session
 from sqlalchemy.engine import Engine
@@ -6,3 +8,21 @@ from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 
 ENGINE_TYPE = Union[Engine, AsyncEngine]
 SESSION_TYPE = Union[Session, AsyncSession]
+
+
+class AsyncDBAPICursor(DBAPICursor):
+    async def execute(
+        self,
+        operation: str,
+        parameters: Union[Sequence[Any], Mapping[str, Any]] = ...,
+        /,
+    ) -> object:
+        ...
+
+
+__all__ = [
+    "ENGINE_TYPE",
+    "SESSION_TYPE",
+    "DBAPICursor",
+    "AsyncDBAPICursor",
+]
