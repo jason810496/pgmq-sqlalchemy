fb0a5417941acedadac5912d5a59671eaca975cd
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-09-12 20:21:53 +0800

    refactor: change to pyformat paramstyle -https://peps.python.org/pep-0249/#paramstyle

--- pgmq_sqlalchemy/_statement.py
+++ pgmq_sqlalchemy/_statement.py
@@ -4,9 +4,11 @@ from typing import List, Tuple
 def create_queue(queue_name: str, unlogged: bool = False) -> Tuple[str, List[str]]:
     """Create a new queue."""
     if unlogged:
-        return "select pgmq.create_unlogged($1);", [queue_name]
+        return "select pgmq.create_unlogged(%(queue_name)s);", {
+            "queue_name": queue_name
+        }
     else:
-        return "select pgmq.create($1);", [queue_name]
+        return "select pgmq.create(%(queue_name)s);", {"queue_name": queue_name}
 
 
 def create_partitioned_queue(
@@ -14,44 +16,66 @@ def create_partitioned_queue(
 ) -> Tuple[str, List[str]]:
     """Create a new partitioned queue."""
     return (
-        "select pgmq.create_partitioned($1, $2, $3);",
-        [queue_name, partition_interval, retention_interval],
+        "select pgmq.create_partitioned(%(queue_name)s, %(partition_interval)s, %(retention_interval)s);",
+        {
+            "queue_name": queue_name,
+            "partition_interval": partition_interval,
+            "retention_interval": retention_interval,
+        },
     )
 
 
 def validate_queue_name(queue_name: str) -> Tuple[str, List[str]]:
     """Validate the length of a queue name."""
-    return "select pgmq.validate_queue_name($1);", [queue_name]
+    return "select pgmq.validate_queue_name(%(queue)s);", {"queue": queue_name}
 
 
 def drop_queue(queue: str, partitioned: bool = False) -> Tuple[str, List[str]]:
     """Drop a queue."""
-    return "select pgmq.drop_queue($1, $2);", [queue, partitioned]
+    return "select pgmq.drop_queue(%(queue)s, %(partitioned)s);", {
+        "queue": queue,
+        "partitioned": partitioned,
+    }
 
 
 def list_queues() -> Tuple[str, List[str]]:
     """List all queues."""
-    return "select queue_name from pgmq.list_queues();", None
+    return "select queue_name from pgmq.list_queues();", {}
 
 
 def send(queue_name: str, message: str, delay: int = 0) -> Tuple[str, List[str]]:
     """Send a message to a queue."""
-    return "select * from pgmq.send($1, $2, $3);", [queue_name, message, delay]
+    return "select * from pgmq.send(%(queue_name)s, %(message)s, %(delay)s);", {
+        "queue_name": queue_name,
+        "message": message,
+        "delay": delay,
+    }
 
 
 def send_batch(queue_name: str, messages: str, delay: int = 0) -> Tuple[str, List[str]]:
     """Send a batch of messages to a queue."""
-    return "select * from pgmq.send_batch($1, $2, $3);", [queue_name, messages, delay]
+    return "select * from pgmq.send_batch(%(queue_name)s, %(messages)s, %(delay)s);", {
+        "queue_name": queue_name,
+        "messages": messages,
+        "delay": delay,
+    }
 
 
 def read(queue_name: str, vt: int) -> Tuple[str, List[str]]:
     """Read a message from a queue."""
-    return "select * from pgmq.read($1, $2, 1);", [queue_name, vt]
+    return "select * from pgmq.read(%(queue_name)s, %(vt)s, 1);", {
+        "queue_name": queue_name,
+        "vt": vt,
+    }
 
 
 def read_batch(queue_name: str, vt: int, batch_size: int) -> Tuple[str, List[str]]:
     """Read a batch of messages from a queue."""
-    return "select * from pgmq.read($1, $2, $3);", [queue_name, vt, batch_size]
+    return "select * from pgmq.read(%(queue_name)s, %(vt)s, %(batch_size)s);", {
+        "queue_name": queue_name,
+        "vt": vt,
+        "batch_size": batch_size,
+    }
 
 
 def read_with_poll(
@@ -59,46 +83,64 @@ def read_with_poll(
 ) -> Tuple[str, List[str]]:
     """Read messages from a queue with polling."""
     return (
-        "select * from pgmq.read_with_poll($1, $2, $3, $4, $5);",
-        [queue_name, vt, qty, max_poll_seconds, poll_interval_ms],
+        "select * from pgmq.read_with_poll(%(queue_name)s, %(vt)s, %(qty)s, %(max_poll_seconds)s, %(poll_interval_ms)s);",
+        {
+            "queue_name": queue_name,
+            "vt": vt,
+            "qty": qty,
+            "max_poll_seconds": max_poll_seconds,
+            "poll_interval_ms": poll_interval_ms,
+        },
     )
 
 
 def pop(queue_name: str) -> Tuple[str, List[str]]:
     """Pop a message from a queue."""
-    return "select * from pgmq.pop($1);", [queue_name]
+    return "select * from pgmq.pop(%(queue_name)s);", {"queue_name": queue_name}
 
 
 def delete(queue_name: str, msg_id: int) -> Tuple[str, List[str]]:
     """Delete a message from a queue."""
-    return "select * from pgmq.delete($1, $2);", [queue_name, msg_id]
+    return "select * from pgmq.delete(%(queue_name)s, %(msg_id)s::BIGINT);", {
+        "queue_name": queue_name,
+        "msg_id": msg_id,
+    }
 
 
 def delete_batch(queue_name: str, msg_ids: List[int]) -> Tuple[str, List[str]]:
     """Delete a batch of messages from a queue."""
-    return "select * from pgmq.delete($1, $2::int[]);", [queue_name, msg_ids]
+    return "select * from pgmq.delete(%(queue_name)s, %(msg_ids)s::BIGINT);", {
+        "queue_name": queue_name,
+        "msg_ids": msg_ids,
+    }
 
 
 def archive(queue_name: str, msg_id: int) -> Tuple[str, List[str]]:
     """Archive a message from a queue."""
-    return "select pgmq.archive($1, $2);", [queue_name, msg_id]
+    return "select pgmq.archive(%(queue_name)s, %(msg_id)s::BIGINT);", {
+        "queue_name": queue_name,
+        "msg_id": msg_id,
+    }
 
 
 def archive_batch(queue_name: str, msg_ids: List[int]) -> Tuple[str, List[str]]:
     """Archive multiple messages from a queue."""
-    return "select pgmq.archive($1, $2::int[]);", [queue_name, msg_ids]
+    return "select * from pgmq.archive(%(queue_name)s, %(msg_ids)s::BIGINT);", {
+        "queue_name": queue_name,
+        "msg_ids": msg_ids,
+    }
 
 
 def purge(queue_name: str) -> Tuple[str, List[str]]:
     """Purge a queue."""
-    return "select pgmq.purge_queue($1);", [queue_name]
+    return "select pgmq.purge_queue(%(queue_name)s);", {"queue_name": queue_name}
 
 
 def metrics(queue_name: str) -> Tuple[str, List[str]]:
     """Get metrics for a queue."""
-    return "select * from pgmq.metrics($1);", [queue_name]
+    return "select * from pgmq.metrics(%(queue_name)s);", {"queue_name": queue_name}
 
 
 def metrics_all() -> Tuple[str, List[str]]:
     """Get metrics for all queues."""
-    return "select * from pgmq.metrics_all();", None
+    return "select * from pgmq.metrics_all();", {}
